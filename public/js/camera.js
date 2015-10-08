f0b6298eb43e89b5a487328c5a2a98f001eae432
Niall Wallace <nj.wallace@outlook.com>
2015-10-08 23:09:10 +0100

    ammedned bug in camera

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
   console.log('Rendered Camera JS')
+  
+  Webcam.set({
+      width: 400,
+      height: 300,
+      dest_width: 640,
+      dest_height: 480,
+      image_format: 'jpeg',
+      jpeg_quality: 90,
+      force_flash: false,
+      flip_horiz: true,
+      fps: 45
+  });
+
+  Webcam.attach( '#my_camera' );
+
+
 })
+
+function take_snapshot() {
+  $('#my_camera').hide()
+    Webcam.snap( function(data_uri) {
+        document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+    } );
+}
